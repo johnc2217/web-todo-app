6e833e449aeb1ac0a18dee8c259b64b1247dfb9b
johnc22 <john@gruppdigital.com>
2023-02-25 16:09:22 -0800

    Added the proc and setup file for deployment on Heroku

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"john.crowell2217@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
